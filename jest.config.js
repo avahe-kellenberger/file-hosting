0b744438dfc27a1abbcef14878e807c64453cbcb
Avahe Kellenberger <avahe@protonmail.ch>
2019-11-23 17:00:07 -0500

    Initial commit.

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+export const moduleFileExtensions = [ 'ts', 'tsx' ];
+export const roots = [ '<rootDir>/test' ];
+export const testRegex = [ '.*\.spec\..*' ];
+export const transform = {
+  '\\.(ts|tsx)$': 'ts-jest'
+};
+
