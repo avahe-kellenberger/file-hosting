c241233f7da1efd2bf2d46421b17976972221b17
Avahe Kellenberger <avahe@protonmail.ch>
2019-11-23 17:28:35 -0500

    Updated configuration.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,20 @@
-export const moduleFileExtensions = [ 'ts', 'tsx' ];
-export const roots = [ '<rootDir>/test' ];
-export const testRegex = [ '.*\.spec\..*' ];
-export const transform = {
-  '\\.(ts|tsx)$': 'ts-jest'
-};
+module.exports = {
+
+  // An array of file extensions your modules use
+  moduleFileExtensions: ['js', 'ts', 'tsx'],
+
+  // A list of paths to directories that Jest should use to search for files in
+  roots: ['<rootDir>/test'],
+
+  // The regexp pattern or array of patterns that Jest uses to detect test files
+  testRegex: ['.*\\.spec.ts'],
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+    '\\.(ts|tsx)$': 'ts-jest'
+  },
+
+  testEnvironment: 'jsdom',
+
+}
 
